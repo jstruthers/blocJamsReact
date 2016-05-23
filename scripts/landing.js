6fe746cb95cb7b61242e4941f459655a6d50cbd0
JochiGolden <jacksonstruthers@gmail.com>
2016-05-23 16:39:30 +0200

    Activate 'points' animation with 'onScroll' event listener

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,50 @@
-var animatePoints = function () {
-
+(function () {
   "use strict";
 
-  var points = document.getElementsByClassName('point'),
-    revealPoints = function () {
-      var i = 0;
+  var pointsArray = document.getElementsByClassName('point'),
+
+    animatePoints = function (points) {
+
+      var revealPoints = function (index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+      },
+        i = 0;
+
       for (i; i < points.length; i += 1) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        revealPoints(i);
       }
+
+    },
+
+    animateHero = function () {
+
+      var heroTitle = document.getElementsByClassName("hero-title");
+
+      heroTitle[0].style.opacity = 1;
+      heroTitle[0].style.letterSpacing = "0.5rem";
+
     };
+  
+  window.onload = function () {
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0],
+      scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-  revealPoints();
-
-},
-
-  animateHero = function () {
-
-    "use strict";
-
-    var heroTitle = document.getElementsByClassName("hero-title");
-
-    heroTitle[0].style.opacity = 1;
-    heroTitle[0].style.letterSpacing = "0.5rem";
-
+    animateHero();
+    
+    if (window.innerHeight > 950) {
+      animatePoints(pointsArray);
+    } else {
+      window.addEventListener('scroll', function (event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          animatePoints(pointsArray);
+        }
+      });
+    }
+    
   };
+
+}());
