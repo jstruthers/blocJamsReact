b2f93e755d091ce9be857961f13b5c7f0ff23a0c
JochiGolden <jacksonstruthers@gmail.com>
2016-05-23 15:42:18 +0200

    Add animations to hero content

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,29 @@
-var animatePoints = function() {
-  
-  var points = document.getElementsByClassName('point');
-  
-  var revealPoints = function() {
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-  };
-  
+var animatePoints = function () {
+
+  "use strict";
+
+  var points = document.getElementsByClassName('point'),
+    revealPoints = function () {
+      var i = 0;
+      for (i; i < points.length; i += 1) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+      }
+    };
+
   revealPoints();
-  
-};
+
+},
+
+  animateHero = function () {
+
+    "use strict";
+
+    var heroTitle = document.getElementsByClassName("hero-title");
+
+    heroTitle[0].style.opacity = 1;
+    heroTitle[0].style.letterSpacing = "0.5rem";
+
+  };
