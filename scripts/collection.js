75f93c1805cf86ced60bf1ecdc7b28e206f77296
JochiGolden <jacksonstruthers@gmail.com>
2016-05-24 08:39:25 +0200

    Add script into collection.html that will generate album templates

---
+++ scripts/collection.js
@@ -0,0 +1,26 @@
+var collectionItemTemplate =
+    '<div class="collection-album-container column fourth">'
+  + '  <img src="assets/images/album_covers/01.png" alt="Album Cover">'
+  + '  <div class="collection-album-info caption">'
+  + '    <p>'
+  + '      <a href="/album.html" class="album-name"> The Colors </a>'
+  + '      <br/>'
+  + '      <a href="/album.html"> Pablo Picasso </a>'
+  + '      <br/>'
+  + '      X Songs'
+  + '      <br/>'
+  + '    </p>'
+  + '  </div>'
+  + '</div>'
+  ;
+
+window.onload = function () {
+  
+  var collectionContainer = document.getElementsByClassName('album-covers')[0];
+  
+  collectionContainer.innerHTML = '';
+  
+  for (var i = 0; i < 12; i += 1) {
+    collectionContainer.innerHTML += collectionItemTemplate;
+  }
+}
